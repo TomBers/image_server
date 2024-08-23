1044aba536b1194b3ce4cef91ebc8ea0e816c60c
TomBers <tjcberman@gmail.com>
2024-08-23 21:43:44 +0100

    looks like it is working

--- lib/image_server/store_s3.ex
+++ lib/image_server/store_s3.ex
@@ -1,27 +1,29 @@
 defmodule ImageServer.StoreS3 do
   alias ExAws.S3
-  @bucket_name "autumn-thunder-1061"
 
-  def store_image(file_name, image_data_base64) do
+  def get_bucket_name do
+    # TODO - create more buckets and return a random one
+    Enum.random(["autumn-thunder-1061"])
+  end
+
+  def store_image(file_name, image_data_base64, bucket_name) do
     image_data_binary = Base.decode64!(image_data_base64, ignore: :whitespace, padding: false)
 
-    S3.put_object(@bucket_name, file_name, image_data_binary, content_type: "image/jpeg")
+    S3.put_object(bucket_name, file_name, image_data_binary, content_type: "image/jpeg")
     |> ExAws.request!()
   end
 
-  def get_url(file_name) do
-    System.get_env() |> IO.inspect()
-    "https://fly.storage.tigris.dev/#{@bucket_name}/#{file_name}"
+  def get_url(file_name, bucket_name) do
+    "https://fly.storage.tigris.dev/#{bucket_name}/#{file_name}"
   end
 
   def test do
     file_name = "encodeTest7.jpg"
     image_data_base64 = ImageServer.TestImage.img()
-
+    bucket_name = get_bucket_name()
     image_data_binary = Base.decode64!(image_data_base64, ignore: :whitespace, padding: false)
-    # File.write!(file_name, image_data_binary)
 
-    S3.put_object(@bucket_name, file_name, image_data_binary, content_type: "image/jpeg")
+    S3.put_object(bucket_name, file_name, image_data_binary, content_type: "image/jpeg")
     |> ExAws.request!()
   end
 end
--- lib/image_server_web/controllers/page_controller.ex
+++ lib/image_server_web/controllers/page_controller.ex
@@ -1,9 +1,13 @@
 defmodule ImageServerWeb.PageController do
   use ImageServerWeb, :controller
 
-  def save_image(conn, %{"file_name" => file_name, "img" => img, "user_id" => _user} = params) do
-    ImageServer.StoreS3.store_image(file_name, img)
-    # TODO - return image_url
-    json(conn, %{img_url: ImageServer.StoreS3.get_url(file_name)})
+  alias ImageServer.StoreS3
+
+  def save_image(conn, %{"file_name" => file_name, "img" => img, "user_id" => _user}) do
+    bucket = StoreS3.get_bucket_name()
+
+    spawn(fn -> StoreS3.store_image(file_name, img, bucket) end)
+
+    json(conn, %{img_url: ImageServer.StoreS3.get_url(file_name, bucket)})
   end
 end
