b1bd09b8a494550653dec682bb5c6a0c0168f6d2
TomBers <tjcberman@gmail.com>
2024-08-23 21:15:53 +0100

    got decode working

--- lib/image_server/store_s3.ex
+++ lib/image_server/store_s3.ex
@@ -2,19 +2,21 @@ defmodule ImageServer.StoreS3 do
   alias ExAws.S3
   @bucket_name "autumn-thunder-1061"
 
-  # def store_image(img) do
-  # bucket_name = "autumn-thunder-1061"
+  def store_image(file_name, image_data_base64) do
+    image_data_binary = Base.decode64!(image_data_base64, ignore: :whitespace, padding: false)
 
-  # ExAws.S3.list_objects(bucket_name) |> ExAws.request() |> IO.inspect()
-  # end
+    S3.put_object(@bucket_name, file_name, image_data_binary, content_type: "image/jpeg")
+    |> ExAws.request!()
+  end
 
   def test do
-    # ExAws.S3.list_objects(@bucket_name) |> ExAws.request() |> IO.inspect()
-    # img = File.open!("interface.jpeg")
-    # {:ok, local_file} = File.read("test.png")
-    test_img = ImageServer.TestImage.img()
+    file_name = "encodeTest7.jpg"
+    image_data_base64 = ImageServer.TestImage.img()
 
-    S3.put_object(@bucket_name, "test3.png", test_img, content_type: "image/png")
+    image_data_binary = Base.decode64!(image_data_base64, ignore: :whitespace, padding: false)
+    # File.write!(file_name, image_data_binary)
+
+    S3.put_object(@bucket_name, file_name, image_data_binary, content_type: "image/jpeg")
     |> ExAws.request!()
   end
 end
--- lib/image_server_web/controllers/page_controller.ex
+++ lib/image_server_web/controllers/page_controller.ex
@@ -1,8 +1,8 @@
 defmodule ImageServerWeb.PageController do
   use ImageServerWeb, :controller
 
-  def save_image(conn, %{"img" => img, "user_id" => _user} = params) do
-    ImageServer.StoreS3.store_image(img)
+  def save_image(conn, %{"file_name" => file_name, "img" => img, "user_id" => _user} = params) do
+    ImageServer.StoreS3.store_image(file_name, img)
     json(conn, params)
   end
 end
