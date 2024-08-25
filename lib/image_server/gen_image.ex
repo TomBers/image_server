0dfee59127fb801045b920cb65c9bcb0474d0036
TomBers <tjcberman@gmail.com>
2024-08-25 22:38:29 +0100

    move image gen code here

---
+++ lib/image_server/gen_image.ex
@@ -0,0 +1,31 @@
+defmodule ImageServer.GenImage do
+  def create_image(api_key, file_name, prompt, bucket_name, save_fn) do
+    headers = [
+      {"Content-Type", "application/json"},
+      {"Authorization", "Bearer #{api_key}"}
+    ]
+
+    body = %{
+      "model" => "dall-e-2",
+      "prompt" => prompt,
+      "n" => 1,
+      "response_format" => "b64_json",
+      "size" => "512x512"
+    }
+
+    response =
+      Req.post!(@api_url,
+        headers: headers,
+        json: body
+      )
+
+    case response.status do
+      200 ->
+        image_data_base64 = response.body["data"] |> List.first() |> Map.get("b64_json")
+        save_fn.(file_name, image_data_base64, bucket_name)
+
+      _ ->
+        IO.puts("Failed to generate image: #{response.status}")
+    end
+  end
+end
--- lib/image_server_web/controllers/page_controller.ex
+++ lib/image_server_web/controllers/page_controller.ex
@@ -3,11 +3,28 @@ defmodule ImageServerWeb.PageController do
 
   alias ImageServer.StoreS3
 
-  def save_image(conn, %{"file_name" => file_name, "img" => img, "user_id" => _user}) do
-    bucket = StoreS3.get_bucket_name()
+  def save_image(conn, %{
+        "file_name" => file_name,
+        "prompt" => prompt,
+        "user_id" => user,
+        "api_key" => api_key
+      }) do
+    if(user == System.fetch_env!("USER_API_KEY")) do
+      bucket = StoreS3.get_bucket_name()
 
-    spawn(fn -> StoreS3.store_image(file_name, img, bucket) end)
+      spawn(fn ->
+        ImageServer.GenImage.create_image(
+          api_key,
+          file_name,
+          prompt,
+          bucket,
+          &StoreS3.store_image/3
+        )
+      end)
 
-    json(conn, %{img_url: ImageServer.StoreS3.get_url(file_name, bucket)})
+      json(conn, %{img_url: ImageServer.StoreS3.get_url(file_name, bucket)})
+    else
+      json(conn, %{error: "Incorrect API Key"})
+    end
   end
 end
